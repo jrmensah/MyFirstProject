8c587c5e7828567b350854b07b72cc26e364ef1f
jrmensah <jmensah4@student.umuc.edu>
2017-10-02 11:42:00 -0400

    First Commit- My first project; Hello Java

---
+++ src/HelloJavaJerilyn.java
@@ -0,0 +1,15 @@
+public class HelloJavaJerilyn {
+    public static void main (String args[])
+    {
+        //Introduction
+        System.out.println("Hello, my name is Jerilyn.");
+        /*Level of experience with Java */
+        System.out.println("My level of experience with Java is Beginner.");
+        /*Major in College */
+        System.out.println("My major in college was Graphic Design.");
+        /*What is interesting about me */
+        System.out.println("I love to write poetry");
+        /*Where I'm from */
+        System.out.println("I am from Gaithersburg, MD.");
+    }
+}
